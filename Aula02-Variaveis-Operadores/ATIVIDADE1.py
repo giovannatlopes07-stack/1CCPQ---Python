00e84259e4674c3a07b7bab848c1426d9253030b
giovannatlopes07 <giovannatlopes07@gmail.com>
2026-03-16 20:45:56 -0300

    Aula 2 - Python

---
+++ Aula02-Variaveis-Operadores/ATIVIDADE1.py
@@ -0,0 +1,17 @@
+#DESAFIO 1
+
+nome = input("Digite o seu nome: ")
+print(f"Bem vindo(a), {nome}")
+
+#DESAFIO 2
+
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nasicmento: ")
+print(f"A data do seu aniversário é: {dia}/{mes}/{ano}")
+
+#DESAFIO 3
+
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+print(f"A soma dos números é: {num1}+{num2}")
---
+++ Aula02-Variaveis-Operadores/Aula02a-variaveis.py
@@ -0,0 +1,26 @@
+#PRINTS
+
+print('Olá, Mundo!')
+print(7+4)
+print('7+4')
+
+
+#Variaveis
+
+nome = 'Giovanna'
+idade = 18
+peso = 47,5
+
+print(nome,idade,peso)
+print(f'Olá, {nome}!')
+
+#IMPUTS --- SIMULAÇÃO DE UM FORMS
+
+nome = input('Digite o seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite o seu peso: ')
+
+print (nome, idade, peso)
+
+
+
