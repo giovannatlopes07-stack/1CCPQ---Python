d13f1ff4c26e7e4af4e5c7019d4841480f051006
giovannatlopes07 <giovannatlopes07@gmail.com>
2026-04-27 20:51:20 -0300

    Aula04

---
+++ Aula04/For.py
@@ -0,0 +1,12 @@
+# for count_musics in range(3):
+#     print(f'Música {count_musics}')
+#
+# #De 1 até 11, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+
+#Atividade 3
+qtd_musicas = int(input('Digite a quantidade de muúsica que você tem na playlist (DB): '))
+
+for i in range(qtd_musicas):
+    print(f'Música {i}')
---
+++ Aula04/Lanços Alinhados.py
@@ -0,0 +1,3 @@
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f'i:{i}, j:{j}')
