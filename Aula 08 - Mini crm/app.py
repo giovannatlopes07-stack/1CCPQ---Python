912aa9229f7d6dd62c536426624f53abed95b11f
giovannatlopes07 <giovannatlopes07@gmail.com>
2026-09-21 20:45:52 -0300

    AULA 09

--- Aula 08 - Mini crm/app.py
+++ Aula 08 - Mini crm/app.py
@@ -1,3 +1,5 @@
+from re import search
+
 from model import model_lead
 import control
 
@@ -20,7 +22,26 @@ def add_lead():
 
 def list_leads():
     leads = control.read_leads()
-    print(leads)
+
+    print(f'## | {"Nome:":<12} | E-mail')
+    for i, lead in enumerate(leads):
+        print(f"{i:02d} | {lead['name']:<12} | {lead['email']}")
+
+def search_leads():
+    query = input('Buscar por: ').strip().lower()
+    # validra query
+
+    search_results = control.read_leads_search(query)
+    print(f'## | {"Nome:":<12} | E-mail')
+    for i, lead in search_results:
+        print(f"{i:02d} | {lead['name']:<12} | {lead['email']}")
+
+def export_leads():
+    path_csv = control.export_csv()
+    if path_csv is None:
+        print("Não foi possível exportar para CSV")
+    else:
+        print(f"Exportado para {path_csv}")
 
 # +1 desafio: formatar como tabela
 
@@ -29,6 +50,8 @@ def main():
         print('/nMini CRM de Leads')
         print('[1] Adicionar lead')
         print('[2] Listar leads')
+        print('[3] Buscar (nome/e-mail)')
+        print('[4] Exportar para CSV')
         print('[0] Sair do progrma')
 
         opt = input('Escolha uma opções')
@@ -37,6 +60,10 @@ def main():
             add_lead()
         elif opt == '2':
             list_leads()
+        elif opt == '3':
+            search_leads()
+        elif opt == '4':
+            export_leads()
         elif opt == '0':
             print('Até mais...')
             break
--- Aula 08 - Mini crm/control.py
+++ Aula 08 - Mini crm/control.py
@@ -1,5 +1,6 @@
+from idlelib import query
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 DB_PATH = DATA_DIR / "leads.json"
@@ -22,4 +23,36 @@ def create_lead(leads_dect):
     leads = read_leads() #Lista
     leads.append(leads_dect)
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+    # CRIAR UMA FORMA PARA NÃO RODAR TODOS OS LEADS QUANDO FORMOS SALVAR UM NOVO
 
+
+# BUSCAR LEADS COM QUERY
+def read_leads_search(query):
+    """Busca Leads de acoedo com o query e RETORNA  uma lista de resultadps"""
+    leads = read_leads()
+    result = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["email"]}". lower()
+
+        if query.lower() in txt_lead:
+            result.append((i, lead))
+
+    return result
+
+#EXPORTAR LEADS PARA CSV
+def export_csv():
+    """Exportar TODOS  os leads para CSV e RETORNA do CSV"""
+    path_csv = DATA_DIR / "leads.csv"
+    leads = read_leads()
+
+    try:
+        with path_csv.open("w", newline="", encoding="utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, leads[0].keys())
+            writer.writeheader()
+            for row_dict in leads:
+                writer.writerow(row_dict)
+
+        return path_csv
+    except PermissionError:
+        return None
